3c8afb5b0db6e4aaa2ac21b29ce66247431c2e9c
stoccop94 <stoccop@stanford.edu>
2023-03-01 18:29:41 -0800

    trying different eol

--- write_dynamicMeshDict.py
+++ write_dynamicMeshDict.py
@@ -1,8 +1,12 @@
 import numpy as np
 from numpy import linspace
+import os
 
 if __name__ == '__main__':
     path = '6Dof_test'
+
+    # Use os.linesep to get the correct end-of-line character for the current OS
+    eol = os.linesep
 
     total_steps = 3
     time_steps = linspace(0,total_steps,total_steps)
@@ -10,9 +14,9 @@ if __name__ == '__main__':
     vel_y = linspace(1,100,total_steps)
     rotation = linspace(1,np.pi,total_steps)
 
-    line_list = [f"\n{total_steps}\n(\n"]
-    line_list.extend([ f"({time_steps[line]} (({vel_x[line]} {vel_y[line]} 0) (0 0 {rotation[line]})))\n" for line in range(total_steps) ])
-    line_list.extend([ f")\n" ])
+    line_list = ["test {}{}".format(total_steps,eol)]
+    #line_list.extend([ f"({time_steps[line]} (({vel_x[line]} {vel_y[line]} 0) (0 0 {rotation[line]})))\n" for line in range(total_steps) ])
+    #line_list.extend([ f")\n" ])
     
     file_lines = ''.join(line_list)
 
