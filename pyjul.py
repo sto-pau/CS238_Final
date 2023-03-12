70ea55bde8d63667db5978993814913b8e698800
stoccop94 <stoccop@stanford.edu>
2023-03-12 14:35:57 -0700

    updated test code to run

--- pyjul.py
+++ pyjul.py
@@ -23,20 +23,19 @@ from julia import Main
 Main.include("linear_model.jl")
 
 model = Main.create_model(6, 3)
-for i in range(200):
+for i in range(50):
     s = np.random.rand(6,1)
     a = random.randint(1,3)
     norm = np.linalg.norm(s)
     if s[0] < 0.5 and a == 1: #stay close to center
         r = 20
     elif s[0] > 0.5 and a == 2: #better to stay in the center
-        r = random.random() * 20 #* (i+1)
+        r = random.random() * 20
     else: #a = don't reward any big motion regardless of state
         r = 0 
     sp = np.random.rand(6,1)
-    print("norm a r:", norm, a, r)
+    print("a r:", a, r)
     Main.update_b(model, s, a, r, sp)
-    #print(model)
 for i in range(10):
     s = np.random.rand(6,1)
     print(s)
