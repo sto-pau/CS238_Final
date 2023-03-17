1cbb7aa2f6ffc2c44db3a93e30705b330dbc2045
zalbasri <zalbasri@stanford.edu>
2023-03-17 11:56:35 -0700

    files in run_files

---
+++ run_files/new_run/s_policy_ucb05.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH -q normal
+#SBATCH -N 1
+#SBATCH -t 20:00:00
+#SBATCH -J  ucb05
+#SBATCH --mail-user=zalbasri@stanford.edu
+#SBATCH --mail-type=ALL
+
+
+module purge
+module load openmpi
+module load anaconda3 
+module load julia 
+eval "$(conda shell.bash hook)"
+conda activate cs238env
+cd /home/zalbasri/CS238/CS238_Final
+python3 ucb05.py 
--- ucb0.2v1test/run_solver.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd backGround
-module load openmpi
-mpirun -np 4 overPimpleDyMFoam -parallel
-#overPimpleDyMFoam
-reconstructPar -latestTime #-newTimes
-foamToVTK -latestTime
-cd ..
